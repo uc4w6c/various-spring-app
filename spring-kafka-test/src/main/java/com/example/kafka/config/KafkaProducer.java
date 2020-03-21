cbe7ff82dad7a58a37e75b695e563be0576495d3
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-03-21 16:48:55 +0900

    kafkaを実行出来た

---
+++ spring-kafka-test/src/main/java/com/example/kafka/config/KafkaProducer.java
@@ -0,0 +1,22 @@
+package com.example.kafka.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Component
+// @EnableConfigurationProperties
+public class KafkaProducer {
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    KafkaProducer(KafkaTemplate<String,String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendKafka(String message) {
+        System.out.println("send:" + message);
+        kafkaTemplate.send("test1" ,message);
+    }
+}
--- spring-kafka-test/src/main/java/com/example/kafka/controller/HelloController.java
+++ spring-kafka-test/src/main/java/com/example/kafka/controller/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.kafka.controller;
 
+import com.example.kafka.config.KafkaProducer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// @EnableConfigurationProperties
 @RequestMapping("/")
 public class HelloController {
+    private KafkaProducer kafkaProducer;
+
+    HelloController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
     @GetMapping
     public String hello() {
+        kafkaProducer.sendKafka("Hello Kafka");
         return "Hello World!";
+    }
+
+    @KafkaListener(id = "client", topics = "test1",groupId = "sample")
+    public void listen(String message) {
+        System.out.println("message:" + message);
     }
 }
