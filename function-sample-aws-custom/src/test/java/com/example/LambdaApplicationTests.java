70e8e7b5ba770bd16ec559bab521a8c6582a00d5
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-12-28 20:58:29 +0900

    修正

---
+++ function-sample-aws-custom/src/test/java/com/example/LambdaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
+
+@FunctionalSpringBootTest
+public class LambdaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
--- mavenlambda/README.md
+++ mavenlambda/README.md
@@ -4,7 +4,7 @@
 serverless deploy
 
 ## curl
-$ curl https://c25eyuf0y2.execute-api.ap-northeast-1.amazonaws.com/dev/testapi?id=11111
+$ curl https://XXXXXXXXXXX.execute-api.ap-northeast-1.amazonaws.com/dev/testapi?id=11111
 {"name":"Taro"}
 
 ## 削除
