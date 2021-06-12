e510b5504b44715fc5b846d9304db970b14a0166
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2021-06-12 23:47:14 +0900

    rest に戻す

--- spring-cors-test/src/main/java/com/example/springcorstest/IndexController.java
+++ spring-cors-test/src/main/java/com/example/springcorstest/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.springcorstest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
