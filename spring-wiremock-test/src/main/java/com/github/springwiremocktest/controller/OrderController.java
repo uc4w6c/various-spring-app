19af6e35c6de203ab4e0eede0af32b21286e7b07
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2022-07-17 21:51:51 +0900

    order APIのテストできた

--- spring-wiremock-test/src/main/java/com/github/springwiremocktest/controller/OrderController.java
+++ spring-wiremock-test/src/main/java/com/github/springwiremocktest/controller/OrderController.java
@@ -1,15 +1,22 @@
 package com.github.springwiremocktest.controller;
 
 import com.github.springwiremocktest.controller.request.OrderPurchaseRequest;
+import com.github.springwiremocktest.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("order")
 public class OrderController {
+  private OrderService orderService;
+
+  public OrderController(OrderService orderService) {
+    this.orderService = orderService;
+  }
+
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public void purchase(@RequestBody OrderPurchaseRequest orderPurchaseRequest) {
-    System.out.println(orderPurchaseRequest);
+    orderService.purchase(orderPurchaseRequest);
   }
 }
--- spring-wiremock-test/src/test/java/com/github/springwiremocktest/integration/OrderControllerIntegrationTest.java
+++ spring-wiremock-test/src/test/java/com/github/springwiremocktest/integration/OrderControllerIntegrationTest.java
@@ -1,39 +1,35 @@
 package com.github.springwiremocktest.integration;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-
+import com.github.springwiremocktest.controller.request.OrderPurchaseRequest;
 import com.github.springwiremocktest.entity.StockEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ProductControllerIntegrationTest extends AbstractIntegrationTest {
+public class OrderControllerIntegrationTest extends AbstractIntegrationTest {
   @Autowired private WebTestClient webTestClient;
 
   @Test
   void 商品を取得できる() {
     int quantity = 10;
     StockEntity stockEntity = new StockEntity("1", quantity);
-    stubFor(get("/stock/products/1").willReturn(jsonResponse(stockEntity, 200)));
+    stubFor(post("/stock/products/reduce").willReturn(created()));
 
+    OrderPurchaseRequest orderPurchaseRequest = new OrderPurchaseRequest("1", 2);
     webTestClient
-        .get()
-        .uri("/products/{id}", "1")
+        .post()
+        .uri("/order/", "1")
+        .body(Mono.just(orderPurchaseRequest), OrderPurchaseRequest.class)
         .accept(MediaType.APPLICATION_JSON)
         .exchange()
-        .expectStatus()
-        .isOk()
-        .expectBody()
-        .jsonPath("$.id")
-        .isEqualTo(1)
-        .jsonPath("$.price")
-        .isEqualTo(1000)
-        .jsonPath("$.quantity")
-        .isEqualTo(quantity);
+        .expectStatus().isCreated();
 
-    verify(1, getRequestedFor(urlEqualTo("/stock/products/1")));
+    verify(1, postRequestedFor(urlEqualTo("/stock/products/reduce")));
   }
 }
