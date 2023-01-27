f1aeef5dc0a8c33bc8abcde5f5d382b55d1e1d19
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2023-01-27 23:04:59 +0900

    gateway firstg

---
+++ spring-gateway-poc/src/test/java/com/example/springgatewaypoc/SpringGatewayPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgatewaypoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
