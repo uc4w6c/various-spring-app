9d3b911d4bb9efc13fa035cf9e2feba880f6eed9
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2023-01-27 23:29:48 +0900

    circuitbreakerとfallbackの作成

--- spring-gateway-poc/src/main/java/com/example/springgatewaypoc/SpringGatewayPocApplication.java
+++ spring-gateway-poc/src/main/java/com/example/springgatewaypoc/SpringGatewayPocApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
 public class SpringGatewayPocApplication {
 
 	public static void main(String[] args) {
@@ -16,9 +20,21 @@ public class SpringGatewayPocApplication {
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(p -> p.path("/get")
-						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("http://httpbin.org:80"))
+				.route(p ->
+						p.path("/get")
+							.filters(f -> f.addRequestHeader("Hello", "World"))
+							.uri("http://httpbin.org:80"))
+				.route(p ->
+						p.host("*.circuitbreaker.com")
+								.filters(f -> f.circuitBreaker(config ->
+										config.setName("mycmd")
+											.setFallbackUri("forward:/fallback")))
+								.uri("http://httpbin.org:80"))
 				.build();
+	}
+
+	@GetMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
 	}
 }
